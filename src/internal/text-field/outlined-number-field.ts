20ff9ad040a42f9a31e22ebc77418fd9684e6afe
Gregory Harder <goharder83@gmail.com>
2024-05-28 12:16:54 -0500

    Added number field internals.

---
+++ src/internal/text-field/outlined-number-field.ts
@@ -0,0 +1,24 @@
+import { customElement } from 'lit/decorators.js';
+import { literal } from 'lit/static-html.js';
+
+import { styles as filledStyles } from '@material/web/textfield/internal/filled-styles.js';
+import { FilledTextField } from '@material/web/textfield/internal/filled-text-field.js';
+
+import { styles as sharedStyles } from './internal/shared-styles.js';
+
+// MARK: Types
+import type { CSSResultOrNative } from 'lit';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-outlined-number-field': MdFilledNumberField;
+  }
+}
+
+// MARK: Code
+
+@customElement('md-outlined-number-field')
+export class MdFilledNumberField extends FilledTextField {
+  static override styles: CSSResultOrNative[] = [sharedStyles, filledStyles];
+  protected override readonly fieldTag = literal`md-filled-field`;
+}
