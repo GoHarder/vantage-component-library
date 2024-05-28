9f0f548c5b0bbc09621faf645c3e5912ab87fbf2
Gregory Harder <goharder83@gmail.com>
2024-05-28 12:12:47 -0500

    Added ripple component.

---
+++ src/ripple/ripple.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import '@material/web/ripple/ripple';
+  import Relay from '../internal/relay.js';
+
+  // MARK: Properties
+  // ------------------------------------------------
+
+  /** Disables the ripple. */
+  export let disabled = false;
+
+  // MARK: Constants
+  // ------------------------------------------------
+
+  const relay = new Relay<'md-ripple', typeof actionProps>();
+
+  // MARK: Reactive Rules
+  // ------------------------------------------------
+
+  $: actionProps = { disabled };
+
+  $: props = Relay.props($$props, ['disabled']);
+
+  // MARK: Lifecycle
+  // ------------------------------------------------
+
+  relay.update = (node, props) => {
+    node.disabled = props.disabled;
+  };
+</script>
+
+<md-ripple use:relay.action={actionProps} {...props} />
