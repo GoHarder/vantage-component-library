31bab5a124b964b6c38c9f2e0026902ddaa598f1
Gregory Harder <goharder83@gmail.com>
2024-05-29 10:40:57 -0500

    Fixed rendering bug.

--- src/internal/text-field/filled-number-field.ts
+++ src/internal/text-field/filled-number-field.ts
@@ -1,12 +1,14 @@
+import '@material/web/field/filled-field.js';
 import { customElement } from 'lit/decorators.js';
 import { literal } from 'lit/static-html.js';
 
 import { styles as filledStyles } from '@material/web/textfield/internal/filled-styles.js';
 import { FilledTextField } from '@material/web/textfield/internal/filled-text-field.js';
-
 import { styles as sharedStyles } from './internal/shared-styles.js';
 
 // MARK: Types
+// ------------------------------------------------
+
 import type { CSSResultOrNative } from 'lit';
 
 declare global {
@@ -16,6 +18,7 @@ declare global {
 }
 
 // MARK: Code
+// ------------------------------------------------
 
 @customElement('md-filled-number-field')
 export class MdFilledNumberField extends FilledTextField {
--- src/internal/text-field/outlined-number-field.ts
+++ src/internal/text-field/outlined-number-field.ts
@@ -1,24 +1,27 @@
+import '@material/web/field/outlined-field.js';
 import { customElement } from 'lit/decorators.js';
 import { literal } from 'lit/static-html.js';
 
-import { styles as filledStyles } from '@material/web/textfield/internal/filled-styles.js';
-import { FilledTextField } from '@material/web/textfield/internal/filled-text-field.js';
-
+import { styles as outlinedStyles } from '@material/web/textfield/internal/outlined-styles.js';
+import { OutlinedTextField } from '@material/web/textfield/internal/outlined-text-field.js';
 import { styles as sharedStyles } from './internal/shared-styles.js';
 
 // MARK: Types
+// ------------------------------------------------
+
 import type { CSSResultOrNative } from 'lit';
 
 declare global {
   interface HTMLElementTagNameMap {
-    'md-outlined-number-field': MdFilledNumberField;
+    'md-outlined-number-field': MdOutlinedNumberField;
   }
 }
 
 // MARK: Code
+// ------------------------------------------------
 
 @customElement('md-outlined-number-field')
-export class MdFilledNumberField extends FilledTextField {
-  static override styles: CSSResultOrNative[] = [sharedStyles, filledStyles];
-  protected override readonly fieldTag = literal`md-filled-field`;
+export class MdOutlinedNumberField extends OutlinedTextField {
+  static override styles: CSSResultOrNative[] = [sharedStyles, outlinedStyles];
+  protected override readonly fieldTag = literal`md-outlined-field`;
 }
