1e17e9a3f9de34d61fbe5e77df86cc07f2df03dc
Gregory Harder <goharder83@gmail.com>
2024-06-19 11:07:24 -0500

    Added context for testing.

--- display/src/index.js
+++ display/src/index.js
@@ -3,6 +3,17 @@
 import './scss/index.scss';
 import App from './svelte/app.svelte';
 
+const reducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+const style = {
+  textInput: { variant: 'outlined' },
+};
+
+/** @type {Map<string, boolean | typeof style>} */
+const context = new Map([
+  ['reducedMotion', reducedMotion],
+  ['style', style],
+]);
+
 const target = document.querySelector('body');
 
-if (target) new App({ target });
+if (target) new App({ target, context });
